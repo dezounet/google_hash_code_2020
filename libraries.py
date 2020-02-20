30074084bf21266dfdb4077dab4cdea88635f6c0
onsefaitchier <0a1z2e3r4t5y6U!>
2020-02-20 19:42:32 +0100

    input process

--- libraries.py
+++ libraries.py
@@ -3,16 +3,17 @@
 
 class Library:
 
-    def __init__(self, books, signing, books_per_day):
+    def __init__(self, book_number, signing, books_per_day):
         """
 
         :param books: list of Books
         :param signing: signing delay (int)
         :param books_per_day: int
         """
-        self.books = books
+        self.books_number = book_number
         self.signing = signing
         self.books_per_day = books_per_day
+        self.book_list = dict()
 
 
 class Book:
--- read.py
+++ read.py
@@ -1,18 +1,49 @@
-SKIP_FIRST_LINE = True
+IS_FIRST_LINE = True
+IS_SECOND_LINE = False
 SEPARATOR = ' '
+
+from libraries import Library
+from libraries import Book
+
 
 def read(file_path):
     objects = []
+    current_library = None
 
     with open(file_path, 'r') as file:
         first_line = True
-        for line in file:
+
+        for index, line in enumerate(file):
             line = line.strip()
 
-            if SKIP_FIRST_LINE and first_line:
-                first_line = False
-            else:
-                elements = list(line.split(SEPARATOR))
-                objects.append(elements)
+            line = list(line.split(SEPARATOR))
+            if IS_FIRST_LINE:
 
-    return objects
+                summary = line
+                IS_FIRST_LINE = False
+                IS_SECOND_LINE = True
+
+            elif IS_SECOND_LINE:
+                book_scores = line
+                IS_SECOND_LINE = False
+
+            else:
+                if index % 2 == 0:
+                    for book in line:
+                        book_tmp = Book()
+                        book_tmp.id = book
+                        book_tmp.score = book_scores[book_tmp.id]
+                        current_library.book_list.append(book_tmp)
+
+                    objects.append(current_library)
+
+                else:
+                    library = Library()
+                    library.books = line[0]
+                    library.signing = line[1]
+                    library.books_per_day = line[2]
+                    current_library = library
+
+
+    return summary, book_scores, objects
+
