aec099186925500065850f4b4ed303ea8bc6d01d
eloi fontaine <eloi.fon10@google.com>
2020-02-20 20:35:07 +0100

    scan books

---
+++ scan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from score_library import score_libraries
+
+
+def scan_books(libraries, method_name):
+
+    # score and sort libraries
+    lib_scores = score_libraries(libraries, method_name)
+    lib_and_scores = zip(libraries, lib_scores)
+    lib_and_scores = sorted(lib_and_scores, key=lambda ls: ls[1])
+
+    sorted_libraries, _ = zip(*lib_and_scores)
+
+    return sorted_libraries
--- score_library.py
+++ score_library.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
-
-def simple_library_score(library):
+def _simple_library_score(library):
 
     b_scores = []
     for b in library.book_list:
         b_scores.append(b.score)
     lib_score = sum(b_scores) / float(len(b_scores)) * library.books_per_day
     return lib_score
+
+def simple_libraries_score(libraries):
+    l_scores = []
+    for l in libraries:
+        l_scores.append(simple_libraries_score())
+    return l_scores
+
+
+SCORE_FN = {
+    'simple_score': simple_libraries_score
+}
+
+
+def score_libraries(libraries, method_name):
+    return SCORE_FN[method_name](libraries)
+
+
+
