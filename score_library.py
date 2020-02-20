4d2c353ae87f8e079dd8275871e17e1324f7bb5f
onsefaitchier <0a1z2e3r4t5y6U!>
2020-02-20 21:12:14 +0100

    update score

--- score_library.py
+++ score_library.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-def _simple_library_score(library):
+import numpy as np
 
+
+def _simple_library_score(library):
     b_scores = []
     for b in library.book_list:
         b_scores.append(b.score)
     lib_score = sum(b_scores) / float(len(b_scores)) * library.books_per_day
     return lib_score
+
 
 def simple_libraries_score(libraries):
     l_scores = []
@@ -24,4 +27,14 @@ def score_libraries(libraries, method_name):
     return SCORE_FN[method_name](libraries)
 
 
+def library_score(library, book_occurences, scanned_books):
+    b_scores = []
+    library_book_occurences = []
+    for index, b in enumerate(library.book_list):
+        b_scores.append(b.score)
+        library_book_occurences[index] = book_occurences[b.id]
+
+    numerator = sum([b_scores[i]/float(library_book_occurences[i]) for i in range(len(b_scores)) if library.book_list[i].id not in scanned_books])*library.books_per_day
+    lib_score = numerator/float(len(b_scores))
+    return lib_score
 
